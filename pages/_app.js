02920762e1a009549bc3a8e77ab9aa1768ff32fa
Amkam <amaduskamara@gmail.com>
2021-10-01 03:32:57 +0000

    Created a bookit nextjs app, configured TailwindCSS and Google fonts montserrat and Open Sans

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+/*
+  1st October, 2021. By Amadu Kamara (Amkam WebAvenger)
+*/
+// Global CSS
+import "../styles/globals.css";
+
+// TailwindCSS
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
