a3c0b14d9a858f286380cdc75df8e0065d2dfd8f
Amkam <amaduskamara@gmail.com>
2021-10-02 03:53:35 +0000

    Created a new room function in the roomControllers for saving new room

--- models/room.js
+++ models/room.js
@@ -7,9 +7,9 @@ const roomSchema = new mongoose.Schema({
     trim: true,
     maxLength: [100, "Room name cannot exceed 100 characters"],
   },
-  price: {
+  pricePerNight: {
     type: Number,
-    required: [true, "Please enter room price"],
+    required: [true, "Please enter room price per night"],
     maxLength: [4, "Room name cannot exceed 4 characters"],
     default: 0.0,
   },
@@ -101,7 +101,7 @@ const roomSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.ObjectId,
     ref: "User",
-    required: true,
+    // required: true,
   },
   createdAt: {
     type: Date,
