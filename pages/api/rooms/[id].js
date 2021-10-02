5e3fa4885d0c9b8772a75c582eb50afc55367b30
Amkam <amaduskamara@gmail.com>
2021-10-02 05:05:42 +0000

    Created updateRoom function for a single room details by it ID

--- pages/api/rooms/[id].js
+++ pages/api/rooms/[id].js
@@ -1,12 +1,16 @@
 import nc from "next-connect";
 import dbConnect from "../../../config/dbConnect";
 
-import { getSingleRoom } from "../../../controllers/roomControllers";
+import {
+  getSingleRoom,
+  updateRoom,
+} from "../../../controllers/roomControllers";
 
 dbConnect();
 
 const handler = nc();
 
 handler.get(getSingleRoom);
+handler.put(updateRoom);
 
 export default handler;
